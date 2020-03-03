969e5fb371a1cfdb3feace70ae5db43b487427a9
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-03-03 14:28:29 +0530

    login card with stepper added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from "@angular/core";
-
-
+import { FormGroup, Validators, FormControl } from "@angular/forms";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  ngOnInit(){}
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.firstFormGroup = new FormGroup({
+      firstCtrl: new FormControl(null, Validators.required)
+    });
+    this.secondFormGroup = new FormGroup({
+      secondCtrl: new FormControl(null, Validators.required)
+    });
+  }
 }
