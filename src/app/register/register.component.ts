083e3a6c903efdef2fe1566b8baad0779b9e105d
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-03-03 17:28:11 +0530

    register working

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,25 +2,43 @@ import { Component, OnInit } from "@angular/core";
 import { FormGroup, Validators, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
-
-
 export class RegisterComponent implements OnInit {
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-
+  isLinear = true;
+  idFormGroup: FormGroup;
+  nameFormGroup: FormGroup;
+  emailFormGroup: FormGroup;
+  passFormGroup: FormGroup;
+  cpassFormGroup: FormGroup;
+  phoneFormGroup: FormGroup;
   constructor() {}
 
   ngOnInit() {
-    this.firstFormGroup = new FormGroup({
-      firstCtrl: new FormControl(null, Validators.required)
+    this.idFormGroup = new FormGroup({
+      id: new FormControl(null, Validators.required)
     });
-    this.secondFormGroup = new FormGroup({
-      secondCtrl: new FormControl(null, Validators.required)
+    this.nameFormGroup = new FormGroup({
+      name: new FormControl(null, Validators.required)
     });
+    this.emailFormGroup = new FormGroup({
+      email: new FormControl(null, Validators.required)
+    });
+    this.passFormGroup = new FormGroup({
+      pass1: new FormControl(null, Validators.required)
+    });
+    this.cpassFormGroup = new FormGroup({
+      pass2: new FormControl(null, Validators.required)
+    });
+    this.phoneFormGroup = new FormGroup({
+      phone: new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    // console.log(this.firstFormGroup.value);
+    // console.log(this.secondFormGroup.value)
   }
 }
