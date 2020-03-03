c5cc2d828bfc565857c5f3b5240579bf93524bac
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-03-03 21:20:33 +0530

    register method added

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,15 +1,32 @@
-import { HttpClient } from "@angular/common/http"
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 import { environment } from "@environments/environment";
 import { UserModel } from "@app/_models";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient){}
+  getAllUsers() {
+    return this.http.get<UserModel[]>(`${environment.apiUrl}/users`);
+  }
 
-    getAllUsers() {
-        return this.http.get<UserModel[]>(`${environment.apiUrl}/users`);
-    }
+  registerUser(
+    id: string,
+    name: string,
+    email: string,
+    pass1: string,
+    pass2: string,
+    phone: string
+  ) {
+    return this.http.post<any>(`${environment.apiUrl}/user/register`, {
+      id,
+      name,
+      email,
+      pass1,
+      pass2,
+      phone
+    });
+  }
 }
