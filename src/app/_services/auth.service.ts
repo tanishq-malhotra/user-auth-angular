c21eb952595c8d7e8b41381ab8453fb169c674fa
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-03-03 18:02:31 +0530

    user auth service added

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,1 +1,38 @@
-export class AuthService {}
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+import { environment } from "@environments/environment";
+import { UserModel } from "@app/_models";
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+  private currentUserSubject: BehaviorSubject<UserModel>;
+  private currentUser: Observable<UserModel>;
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<UserModel>(
+      JSON.parse(localStorage.getItem("cs"))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): UserModel {
+    return this.currentUserSubject.value;
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<any>(`${environment.apiUrl}/users/authenticate`, {
+        email,
+        password
+      })
+      .pipe(
+        map(user => {
+          localStorage.setItem("cs", JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
+        })
+      );
+  }
+}
