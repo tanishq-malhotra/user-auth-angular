34a3528ef9dd380700272e0065a16d7188e913e7
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-03-03 18:02:43 +0530

    user get service added

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,1 +1,15 @@
-export class UserService {}
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from '@angular/core';
+
+import { environment } from "@environments/environment";
+import { UserModel } from "@app/_models";
+
+@Injectable()
+export class UserService {
+
+    constructor(private http: HttpClient){}
+
+    getAllUsers() {
+        return this.http.get<UserModel[]>(`${environment.apiUrl}/users`);
+    }
+}
