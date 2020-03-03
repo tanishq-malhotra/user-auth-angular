ef369f6102dad7c941c0eb71cb59f8ece9f2de1c
tanishq-malhotra <malhotra.tanishq6785@gmail.com>
2020-03-03 20:41:28 +0530

    logout added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
+import { Subscription } from "rxjs";
+import { UserModel } from "@app/_models";
+import { AuthService } from "@app/_services";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent implements OnInit {
-  ngOnInit() {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  currentUser: UserModel;
+  userSubscription: Subscription;
+  constructor(private authService: AuthService) {}
+  ngOnInit() {
+    this.userSubscription = this.authService.currentUser.subscribe(
+      user => (this.currentUser = user)
+    );
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
 }
